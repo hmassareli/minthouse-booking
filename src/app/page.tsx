744d41095ee891868ed232b5471701ed4f8a32e5
Henrique Massareli <henrique@oneseventech.com>
2024-02-16 21:42:26 -0300

    testing weird things

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,6 +27,7 @@ export default function Home() {
   const setDate = useSetAtom(dateAtom);
 
   const handleChangeCalendar = (newValue: Date | null) => {
+    console.log(newValue);
     setDate(newValue);
     setSelectedDateString(newValue);
     if (newValue) {
@@ -45,7 +46,7 @@ export default function Home() {
           }
         >
           <h1 className="text-[27px] mt-0 font-extrabold mb-10 leading-none ">
-            Book a Court
+            Book a Court {"isDaySelected" + isDaySelected ? "Yes" : "No"}
           </h1>
           <div className="mb-4">
             <h2 className="mb-[5px] text-[1.2rem]">Location</h2>
