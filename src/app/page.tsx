0bdeb7441e24bf381e204e9fc04bfadb43705d39
Henrique Massareli <henrique@oneseventech.com>
2024-02-16 23:40:27 -0300

    fix: try to fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,6 @@ import { getAvailableSlots } from "@/utils";
 import { MenuItem } from "@mui/material";
 import dayjs from "dayjs";
 import { useAtom, useSetAtom } from "jotai";
-import { useRouter } from "next/navigation";
 import { useState } from "react";
 import BasicDateCalendar from "./../components/DatePicker";
 
@@ -34,7 +33,6 @@ export default function Home() {
     }
   };
 
-  const router = useRouter();
   return (
     <main className=" flex min-h-screen flex-col items-center justify-center">
       <Card className="main-card">
@@ -148,7 +146,7 @@ export default function Home() {
                         {formattedTime}
                       </div>
                       <div
-                        onClick={() => router.push("/user-info")}
+                        onClick={() => {}}
                         className={
                           (!isSlotSelected && "hidden") +
                           " h-16 flex transition-all duration-75 justify-center rounded-md items-center cursor-pointer hover:bg-[#b7c050] text-white font-medium bg-[#D7E164] w-1/2"
